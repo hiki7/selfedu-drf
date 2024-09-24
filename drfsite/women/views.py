69df6ff559ff88f8bce025882c1e0074a4ab6163
hiki7 <rsaliyev03@gmail.com>
2024-09-24 18:56:07 +0500

    add: view using APIView

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Women, Category
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all()
+        return Response({'posts': list(lst)})
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
