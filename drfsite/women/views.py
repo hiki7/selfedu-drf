fc1418ff7d62cc75a34df023508665299e014f2d
hiki7 <rsaliyev03@gmail.com>
2024-09-24 19:14:27 +0500

    add: urls are configured and post method for women api

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
@@ -8,8 +9,16 @@ from .models import Women, Category
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all()
+        lst = Women.objects.all().values()
         return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
