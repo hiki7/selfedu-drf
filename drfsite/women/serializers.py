575dc07b8486161174734253bebed2231078eda1
hiki7 <rsaliyev03@gmail.com>
2024-09-24 19:34:47 +0500

    change: write the women serializer using Serializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 from drfsite.women.models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ['title', 'content', 'time_create', 'time_update', 'is_published', 'cat']
+class WomenSerialzier(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(auto_now_add=True)
+    time_update = serializers.DateTimeField(auto_now=True)
+    is_published = serializers.BooleanField(default=True)
+    cat = serializers.IntegerField()
+
+# class WomenSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Women
+#         fields = ['title', 'content', 'time_create', 'time_update', 'is_published', 'cat']
