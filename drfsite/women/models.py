d58377052fd7b3d3f7501f34d713015c04f7e32b
hiki7 <rsaliyev03@gmail.com>
2024-09-24 18:31:19 +0500

    fix: misspelling of word 'length'

--- drfsite/women/models.py
+++ drfsite/women/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Women(models.Model):
-    title = models.CharField(max_lenght=255)
+    title = models.CharField(max_length=255)
     content = models.TextField()
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
