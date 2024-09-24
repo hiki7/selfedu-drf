0826f0c7b97310d52e5ae60433a4482f9769bbea
hiki7 <rsaliyev03@gmail.com>
2024-09-24 18:28:13 +0500

    project template

---
+++ drfsite/women/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Women(models.Model):
+    title = models.CharField(max_lenght=255)
+    content = models.TextField()
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
